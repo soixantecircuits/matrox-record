3dbd98d13e4e28c6c2c3f0219e432c56f9342002
gabrielstuff <gabriel@soixantecircuits.fr>
2015-12-07 18:49:10 +0100

    adding new api usage and update module

--- README.md
+++ README.md
@@ -1,11 +1,34 @@
-# Matrox record
+# Matrox Monarch record
+
+Interactive command-line based on the following documentation:
+ftp://ftp.matrox.com/pub/video/doc/Matrox%20Monarch%20HDX%20Dev%20Tools%20Reference%20Guide_1.1.1.pdf
+
+Previous version was based on:
+http://www.matrox.com/video/media/pdf/support/monarch_hd/doc/en_Matrox_Monarch_HD_Control_API_Guide_1_1_3.pdf
 
 ## Installation
 
-1. Run `npm install`
+0. Clone :
+`git clone git@github.com:soixantecircuits/matrox-record.git`
+
+1. Install
+`npm install`
 
 2. configure by modifying `config.json` file.
 
-3. and then `npm start` 
+3. configure your option there :
 
-Make sure you are pointing to the right IP.
+```
+options = {
+    timeout: 3000,
+    permanentTestInterval: 60000,
+    recordDuration: 15000,
+    hdx: true
+  },
+```
+
+4. And start with: `npm start`
+
+Make sure you are pointing to the right IP of you Matrox Monarch HD or HDX
+
+You can select, the `hdx` option with `true` or `false`
--- app.js
+++ app.js
@@ -1,69 +1,153 @@
-config = require('./config/config.json');
-var configurl = "http://<username>:<password>@<ipAddress>/Monarch/syncconnect/sdk.aspx?command=<command>",
-secureurl = configurl.replace('<username>', config.username).replace('<password>', config.password).replace('<ipAddress>', config.ipAddress),
-request = require('request'),
-colors = require('colors'),
+'use strict';
 
-getStatus = function (){
-  var url = secureurl.replace('<command>', 'GetStatus');
-  console.log(url);
-  request(url, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log('Status: ' + body.blue);
+var config = require('./config/config.json'),
+  configurl = "http://<username>:<password>@<ipAddress>/Monarch/syncconnect/sdk.aspx?command=<command>",
+  secureurl = configurl.replace('<username>', config.username).replace('<password>', config.password).replace('<ipAddress>', config.ipAddress),
+  request = require('request'),
+  colors = require('colors'),
+  inquirer = require('inquirer'),
+  options = {
+    timeout: 3000,
+    permanentTestInterval: 60000,
+    recordDuration: 15000,
+    hdx: true
+  },
+  encoderOptions = ['StartRecording', 'StartEncoder1', 'StartEncoder2', 'StartBothEncoders'],
+  stopOptions = ['StartRecording', 'StopEncoder1', 'StopEncoder2', 'StopBothEncoders'],
+  permanentTest,
+
+  getStatus = function() {
+    var url = secureurl.replace('<command>', 'GetStatus');
+    console.log(url);
+    request({
+      url: url,
+      timeout: options.timeout
+    }, function(error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log('Status: ' + body.blue);
+      } else {
+        if (error.code === 'ETIMEDOUT') {
+          console.error('Can\'t reach host. Please verify your config or that the address is well configured.\n');
+          process.exit();
+        } else {
+          console.log('Status error: ', error);
+        }
+      }
+    });
+  },
+
+
+  startRecord = function(encodeIndex) {
+    if (encodeIndex < encoderOptions.length && encodeIndex > -1) {
+      var url = secureurl.replace('<command>', encoderOptions[encodeIndex]);
+      console.log('Start record with:', url);
+      request({
+        url: url,
+        timeout: options.timeout
+      }, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+          console.log('Recording: ' + body.green);
+          setTimeout(function() {
+            stopRecord();
+          }, options.recordDuration);
+        } else {
+          console.log('Error, starting record: ' + error.red);
+        }
+      });
     } else {
-      console.log(error);
+      console.log('the stopOptions is too short or too long...');
+    }
+  },
+
+  stopRecord = function(stopIndex) {
+    if (stopIndex < stopOptions.length && stopIndex > -1) {
+      var url = secureurl.replace('<command>', stopOptions[stopIndex]);
+      console.log('Stop record with:', url);
+      request({
+        url: url,
+        timeout: options.timeout
+      }, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+          console.log('Stop recording: ' + body.green);
+        } else {
+          console.log('Error, stoping record: ' + error.red);
+        }
+      });
+    } else {
+      console.log('the stopOptions is too short or too long...');
+    }
+  },
+
+  testCall = function(optionIndex) {
+    startRecord(optionIndex);
+  },
+
+  runPermanentTest = function(optionsNumber) {
+    clearInterval(permanentTest);
+    permanentTest = setInterval(function() {
+      testCall(optionsNumber);
+    }, options.permanentTestInterval);
+  },
+
+  stopPermanenTest = function() {
+    clearInterval(permanentTest);
+  };
+
+var questions = [
+  {
+    type: "list",
+    name: "command",
+    message: "What do you want to do?",
+    choices: [
+      "Get status for Matrox HD",
+      "Start recording",
+      "Stop recording",
+      "Run permanent test",
+      "Stop permanent test",
+      "Exit"
+    ]
+  }
+];
+
+function ask() {
+  inquirer.prompt(questions, function(answers) {
+    if (answers.command === 'Exit') {
+      process.exit();
+    } else {
+      var optionIndex = 0;
+          if (options.hdx) {
+            optionIndex = 3;
+          } else {
+            optionIndex = 0
+          }
+      switch (answers.command) {
+        case 'Get status for Matrox HD':
+          console.log('Getting status...');
+          getStatus();
+          break;
+        case 'Start recording':
+          console.log('Start recording for ' + options.recordDuration + ' Do not forget to stop it!');
+          startRecord(optionIndex);
+          break;
+        case 'Stop recording':
+          console.log('Stop recording...');
+          stopRecord(optionIndex);
+          break;
+        case 'Run permanent test':
+          console.log('Run permanent test based on every ' + options.permanentTestInterval + 'ms');
+          runPermanentTest(optionIndex);
+          break;
+        case 'Stop permanent test':
+          console.log('Stoping test...');
+          stopPermanenTest();
+          break;
+        default:
+          console.log("Wtf ???");
+      }
+      ask();
     }
   });
-},
+}
 
-record = function(){
-  var url = secureurl.replace('<command>', 'StartRecording');
-  console.log(url);
-  request(url, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log('Recording: ' + body.green);
-      setTimeout(function(){
-        stop();
-      }, 15000)
-    } else {
-      console.log('Error, starting record: ' + error.red);
-    }
-  });
-},
+ask();
 
-stop = function(){
-  var url = secureurl.replace('<command>', 'StopRecording');
-  console.log(url);
-  request(url, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log('Stop recording: ' + body.green);
-    } else {
-      console.log('Error, stoping record: ' + error.red);
-    }
-  });
-},
-
-test = function(){
-  record();
-};
-
-var readline = require('readline');
-
-var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-rl.question("What do you think of node.js? ", function(answer) {
-  // TODO: Log the answer in a database
-  console.log("Thank you for your valuable feedback:", answer);
-  rl.close();
-});
-
-getStatus();
-
-test();
-
-setInterval(function(){
-  test();
-}, 60000);
