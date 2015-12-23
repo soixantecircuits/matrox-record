6b45f2a55d71045655d5e4a852df7b2aef0d3bba
gabrielstuff <gabriel@soixantecircuits.fr>
2015-12-23 12:12:02 +0100

    moving to standars-js

--- app.js
+++ app.js
@@ -1,153 +1,150 @@
-'use strict';
-
-var config = require('./config/config.json'),
-  configurl = "http://<username>:<password>@<ipAddress>/Monarch/syncconnect/sdk.aspx?command=<command>",
-  secureurl = configurl.replace('<username>', config.username).replace('<password>', config.password).replace('<ipAddress>', config.ipAddress),
-  request = require('request'),
-  colors = require('colors'),
-  inquirer = require('inquirer'),
-  options = {
+'use strict'
+var config = require('./config/config.json')
+var configurl = 'http://<username>:<password>@<ipAddress>/Monarch/syncconnect/sdk.aspx?command=<command>'
+var secureurl = configurl.replace('<username>', config.username).replace('<password>', config.password).replace('<ipAddress>', config.ipAddress)
+var request = require('request')
+var colors = require('colors')
+var inquirer = require('inquirer')
+var options = {
     timeout: 3000,
     permanentTestInterval: 60000,
     recordDuration: 15000,
     hdx: true
-  },
-  encoderOptions = ['StartRecording', 'StartEncoder1', 'StartEncoder2', 'StartBothEncoders'],
-  stopOptions = ['StartRecording', 'StopEncoder1', 'StopEncoder2', 'StopBothEncoders'],
-  permanentTest,
+  }
+var encoderOptions = ['StartRecording', 'StartEncoder1', 'StartEncoder2', 'StartBothEncoders']
+var stopOptions = ['StartRecording', 'StopEncoder1', 'StopEncoder2', 'StopBothEncoders']
+var permanentTest
 
-  getStatus = function() {
-    var url = secureurl.replace('<command>', 'GetStatus');
-    console.log(url);
+  getStatus = function () {
+    var url = secureurl.replace('<command>', 'GetStatus')
+    console.log(url)
     request({
       url: url,
       timeout: options.timeout
-    }, function(error, response, body) {
+    }, function (error, response, body) {
       if (!error && response.statusCode == 200) {
-        console.log('Status: ' + body.blue);
+        console.log('Status: ' + body.blue)
       } else {
         if (error.code === 'ETIMEDOUT') {
-          console.error('Can\'t reach host. Please verify your config or that the address is well configured.\n');
-          process.exit();
+          console.error("Can't reach host. Please verify your config or that the address is well configured.\n")
+          process.exit()
         } else {
-          console.log('Status error: ', error);
+          console.log('Status error: ', error)
         }
       }
-    });
+    })
   },
 
-
-  startRecord = function(encodeIndex) {
+  startRecord = function (encodeIndex) {
     if (encodeIndex < encoderOptions.length && encodeIndex > -1) {
-      var url = secureurl.replace('<command>', encoderOptions[encodeIndex]);
-      console.log('Start record with:', url);
+      var url = secureurl.replace('<command>', encoderOptions[encodeIndex])
+      console.log('Start record with:', url)
       request({
         url: url,
         timeout: options.timeout
-      }, function(error, response, body) {
+      }, function (error, response, body) {
         if (!error && response.statusCode == 200) {
-          console.log('Recording: ' + body.green);
-          setTimeout(function() {
-            stopRecord();
-          }, options.recordDuration);
+          console.log('Recording: ' + body.green)
+          setTimeout(function () {
+            stopRecord()
+          }, options.recordDuration)
         } else {
-          console.log('Error, starting record: ' + error.red);
+          console.log('Error, starting record: ' + error.red)
         }
-      });
+      })
     } else {
-      console.log('the stopOptions is too short or too long...');
+      console.log('the stopOptions is too short or too long...')
     }
   },
 
-  stopRecord = function(stopIndex) {
+  stopRecord = function (stopIndex) {
     if (stopIndex < stopOptions.length && stopIndex > -1) {
-      var url = secureurl.replace('<command>', stopOptions[stopIndex]);
-      console.log('Stop record with:', url);
+      var url = secureurl.replace('<command>', stopOptions[stopIndex])
+      console.log('Stop record with:', url)
       request({
         url: url,
         timeout: options.timeout
-      }, function(error, response, body) {
+      }, function (error, response, body) {
         if (!error && response.statusCode == 200) {
-          console.log('Stop recording: ' + body.green);
+          console.log('Stop recording: ' + body.green)
         } else {
-          console.log('Error, stoping record: ' + error.red);
+          console.log('Error, stoping record: ' + error.red)
         }
-      });
+      })
     } else {
-      console.log('the stopOptions is too short or too long...');
+      console.log('the stopOptions is too short or too long...')
     }
   },
 
-  testCall = function(optionIndex) {
-    startRecord(optionIndex);
+  testCall = function (optionIndex) {
+    startRecord(optionIndex)
   },
 
-  runPermanentTest = function(optionsNumber) {
-    clearInterval(permanentTest);
-    permanentTest = setInterval(function() {
-      testCall(optionsNumber);
-    }, options.permanentTestInterval);
+  runPermanentTest = function (optionsNumber) {
+    clearInterval(permanentTest)
+    permanentTest = setInterval(function () {
+      testCall(optionsNumber)
+    }, options.permanentTestInterval)
   },
 
-  stopPermanenTest = function() {
-    clearInterval(permanentTest);
-  };
+  stopPermanenTest = function () {
+    clearInterval(permanentTest)
+  }
 
 var questions = [
   {
-    type: "list",
-    name: "command",
-    message: "What do you want to do?",
+    type: 'list',
+    name: 'command',
+    message: 'What do you want to do?',
     choices: [
-      "Get status for Matrox HD",
-      "Start recording",
-      "Stop recording",
-      "Run permanent test",
-      "Stop permanent test",
-      "Exit"
+      'Get status for Matrox HD',
+      'Start recording',
+      'Stop recording',
+      'Run permanent test',
+      'Stop permanent test',
+      'Exit'
     ]
   }
-];
+]
 
-function ask() {
-  inquirer.prompt(questions, function(answers) {
+function ask () {
+  inquirer.prompt(questions, function (answers) {
     if (answers.command === 'Exit') {
-      process.exit();
+      process.exit()
     } else {
-      var optionIndex = 0;
-          if (options.hdx) {
-            optionIndex = 3;
-          } else {
-            optionIndex = 0
-          }
+      var optionIndex = 0
+      if (options.hdx) {
+        optionIndex = 3
+      } else {
+        optionIndex = 0
+      }
       switch (answers.command) {
         case 'Get status for Matrox HD':
-          console.log('Getting status...');
-          getStatus();
-          break;
+          console.log('Getting status...')
+          getStatus()
+          break
         case 'Start recording':
-          console.log('Start recording for ' + options.recordDuration + ' Do not forget to stop it!');
-          startRecord(optionIndex);
-          break;
+          console.log('Start recording for ' + options.recordDuration + ' Do not forget to stop it!')
+          startRecord(optionIndex)
+          break
         case 'Stop recording':
-          console.log('Stop recording...');
-          stopRecord(optionIndex);
-          break;
+          console.log('Stop recording...')
+          stopRecord(optionIndex)
+          break
         case 'Run permanent test':
-          console.log('Run permanent test based on every ' + options.permanentTestInterval + 'ms');
-          runPermanentTest(optionIndex);
-          break;
+          console.log('Run permanent test based on every ' + options.permanentTestInterval + 'ms')
+          runPermanentTest(optionIndex)
+          break
         case 'Stop permanent test':
-          console.log('Stoping test...');
-          stopPermanenTest();
-          break;
+          console.log('Stoping test...')
+          stopPermanenTest()
+          break
         default:
-          console.log("Wtf ???");
+          console.log('Wtf ???')
       }
-      ask();
+      ask()
     }
-  });
+  })
 }
-
-ask();
-
+//let's ask the user what he wants !
+ask()
